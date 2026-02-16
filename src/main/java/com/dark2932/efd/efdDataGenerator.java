6f99e0c173f26952e7a01faec8a5ea31230fdfb7
Murchey <2371709684@qq.com>
2026-02-16 18:59:45 +0800

    datagen

---
+++ src/main/java/com/dark2932/efd/efdDataGenerator.java
@@ -0,0 +1,31 @@
+package com.dark2932.efd;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.dark2932.efd.datagen.efdBlockTablesProvider;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = EFD.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class efdDataGenerator {
+    public static void gatherData(GatherDataEvent event){
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(efdBlockTablesProvider::new, LootContextParamSets.BLOCK)
+        )));
+    }
+
+}
