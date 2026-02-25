b81f19e100dc38db693b82645ba033becd6fdfa3
Murchey <2371709684@qq.com>
2026-02-25 14:34:34 +0800

    修正文件名

--- src/main/java/com/dark2932/efd/EFDDataGenerator.java
+++ src/main/java/com/dark2932/efd/EFDDataGenerator.java
@@ -4,18 +4,16 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.fml.common.Mod;
-import com.dark2932.efd.datagen.efdBlockModelProvider;
 
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = EFD.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class efdDataGenerator {
+public class EFDDataGenerator {
     public static void gatherData(GatherDataEvent event){
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
--- src/main/java/com/dark2932/efd/datagen/EFDBlockModelProvider.java
+++ src/main/java/com/dark2932/efd/datagen/EFDBlockModelProvider.java
@@ -1,13 +1,12 @@
 package com.dark2932.efd.datagen;
 
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraftforge.client.model.generators.BlockModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-public class efdBlockModelProvider extends BlockModelProvider {
+public class EFDBlockModelProvider extends BlockModelProvider {
 
-    public efdBlockModelProvider(PackOutput output, String modid, ExistingFileHelper existingFileHelper) {
+    public EFDBlockModelProvider(PackOutput output, String modid, ExistingFileHelper existingFileHelper) {
         super(output, modid, existingFileHelper);
     }
 
