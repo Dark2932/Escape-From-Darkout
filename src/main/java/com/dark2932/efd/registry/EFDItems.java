f0bf1133c8b0f836601e3a732ef817fb86d2d136
Dark2932 <lqy070821@163.com>
2026-02-25 17:04:53 +0800

    update

--- src/main/java/com/dark2932/efd/registry/EFDItems.java
+++ src/main/java/com/dark2932/efd/registry/EFDItems.java
@@ -26,30 +26,32 @@ public class EFDItems {
     public static final ItemEntry STEEL_BOTTLE = ITEM_REGISTER.newItem("steel_bottle");
 
     /** 食物与饮品 **/
-    public static final ItemEntry TAURINE_DRINK = newFoodDrink("taurine_drink", 16, STEEL_BOTTLE,
+    public static final ItemEntry TAURINE_DRINK = newFoodDrink("taurine_drink", STEEL_BOTTLE,
             new Item.Properties().stacksTo(16),
             (newFoodProps().nutrition(3).saturationMod(5.5f)
                     .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 600), 1.0f)
                     .fast()
-                    .build()
+                    .alwaysEat()
             )
     );
 
     /** 构建物品的私有方法 **/
-    private static ItemEntry newDrink(String name, int duration, Item.Properties properties) {
-        return ITEM_REGISTER.newItem(name, () -> new DrinkableItem(properties).setDrinkDuration(duration == -1 ? 32 : duration));
+    private static ItemEntry newDrink(String name, boolean isFast, Item.Properties properties) {
+        return ITEM_REGISTER.newItem(name, () -> new DrinkableItem(properties).setDrinkDuration(isFast ? 16 : 32));
     }
 
-    private static ItemEntry newDrink(String name, int duration, ItemEntry container, Item.Properties properties) {
-        return ITEM_REGISTER.newItem(name, () -> new DrinkableItem(properties).setDrinkDuration(duration == -1 ? 32 : duration).setContainer(container.item()));
+    private static ItemEntry newDrink(String name, boolean isFast, ItemEntry container, Item.Properties properties) {
+        return ITEM_REGISTER.newItem(name, () -> new DrinkableItem(properties).setDrinkDuration(isFast ? 16 : 32).setContainer(container.item()));
     }
 
-    private static ItemEntry newFoodDrink(String name, int duration, Item.Properties properties, FoodProperties foodProperties) {
-        return newDrink(name, duration, properties.food(foodProperties));
+    private static ItemEntry newFoodDrink(String name, Item.Properties properties, FoodProperties.Builder builder) {
+        FoodProperties foodProperties = builder.build();
+        return newDrink(name, foodProperties.isFastFood(), properties.food(foodProperties));
     }
 
-    private static ItemEntry newFoodDrink(String name, int duration, ItemEntry container, Item.Properties properties, FoodProperties foodProperties) {
-        return newDrink(name, duration, container, properties.food(foodProperties));
+    private static ItemEntry newFoodDrink(String name, ItemEntry container, Item.Properties properties, FoodProperties.Builder builder) {
+        FoodProperties foodProperties = builder.build();
+        return newDrink(name, foodProperties.isFastFood(), container, properties.food(foodProperties));
     }
 
     private static FoodProperties.Builder newFoodProps() {
